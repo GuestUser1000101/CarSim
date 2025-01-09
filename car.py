Fix incorrect torque calculation

car.py
@@ -38,7 +38,7 @@ class Car:
 
     def apply_force_at_position(self, force, position, delta_time):
         self.apply_force(force, delta_time)
-        torque = np.linalg.norm(force) * np.linalg.norm(position) * math.sin(get_signed_angle_between(force, -position))
+        torque = np.linalg.norm(force) * np.linalg.norm(position) * math.sin(get_signed_angle_between(-position, force))
         self.rotational_velocity += torque / self.rotational_inertia * delta_time
 
     def apply_force(self, force, delta_time):
@@ -65,7 +65,7 @@ class Car:
         self.steering_angle = get_deadband(self.steering_angle, 0.001)
 
         if self.steering_angle != 0:
-            turn_radius = -self.length / math.tan(self.steering_angle)
+            turn_radius = self.length / math.tan(self.steering_angle)
 
         for wheel, is_steering in zip(self.wheels, self.steering_wheels):
             wheel.update_physics(delta_time)
@@ -85,8 +85,10 @@ class Car:
     
     def draw(self, screen):
         draw_rect(screen, (255, 0, 0), self.position, self.length, self.width, self.rotation, self.world.pixels_per_meter)
+        draw_vector(screen, (0, 255, 0), self.position, self.velocity, 2, 0.1, self.world.pixels_per_meter)
         for wheel in self.wheels:
             draw_rect(screen, (0, 255, 255), self.position + rotate_vector(wheel.position, self.rotation), 1, 0.4, self.rotation + wheel.rotation, self.world.pixels_per_meter)
+            draw_vector(screen, (255, 255, 255), self.position + rotate_vector(wheel.position, self.rotation), rotate_vector(wheel.steering_direction, self.rotation), 1, 1, self.world.pixels_per_meter)
     
     def draw_steering(self, screen, screen_x, screen_y):
         draw_rect(screen, (255, 255, 255), np.array([screen_x, screen_y]), 60, 75, self.steering_angle)

wheel.py
@@ -12,7 +12,7 @@ class Wheel:
         self.velocity = np.array((0.0, 0.0, 0.0))
         self.rotation = 0
         self.mass = mass
-
+        
         # Suspension
         self.spring_strength = spring_strength
         self.damping_strength = damping_strength
@@ -20,6 +20,8 @@ class Wheel:
         # Traction
         self.traction_function = self.default_traction_function if traction_function == None else traction_function
         self.wheel_friction = 100
+        self.steering_force = np.array((0.0, 0.0, 0.0))
+        self.steering_direction = np.array((0.0, 0.0, 0.0))
 
         # Drive
         self.power = 0
@@ -31,11 +33,13 @@ class Wheel:
         return np.array([0, 0, vertical_force])
     
     def get_steering_force(self, delta_time):
-        steering_direction = np.array([math.cos(self.rotation), math.sin(self.rotation), 0])
+        steering_direction = np.array([math.cos(-self.rotation), math.sin(-self.rotation), 0])
+        self.steering_direction = steering_direction
+
         global_velocity = (
             rotate_vector(
                 self.car.rotational_velocity * self.position,
-                -math.pi / 2
+                math.pi / 2
             ) +
             rotate_vector(self.car.velocity, -self.car.rotation) +
             self.velocity
@@ -49,7 +53,8 @@ class Wheel:
         traction = self.traction_function(abs(steering_velocity / np.linalg.norm(global_velocity)))
         # removing wheel_friction - it is just traction
         # removing traction - unecessary complexity, add back later
-        return -steering_velocity / delta_time * self.car.mass / 4 * steering_direction #* self.wheel_friction * traction 
+        self.steering_force = -steering_velocity / delta_time * self.car.mass / 4 * steering_direction #* self.wheel_friction * traction
+        return self.steering_force
         
     def get_driving_force(self, deltaTime):
         if self.power == 0: